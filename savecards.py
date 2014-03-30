117b7734dc795020d0aa8d3fbeeab423fe7d5a1d
Alan Kraut <Alan.Kraut@gmail.com>
2014-03-29 22:37:29 -0700

    Making json structure. Not getting info from client yet.

--- savecards.py
+++ savecards.py
@@ -1,13 +1,43 @@
 from Tkinter import *
+import json
 import tkFileDialog
 
-def save():
+def save():#(cards):
+  # temporary data for testing. Delete this soon and reinstate the input
+  cards = ['card 1',
+           'card 3',
+           'card 0',
+           'card 3',
+           'card 8',
+           'card 15',
+           'card 8',
+           'card 3']
   # make some sort of save dialog??
-  f = tkFileDialog.asksaveasfile(mode='w', defaultextension=".xml")
+  f = tkFileDialog.asksaveasfile(mode='w', defaultextension=".json")
   if f is None:
     return
 
-  text = "This is TOTALLY an XML file full of magic cards."
+  # temporary structure to keep track of what cards we have seen
+  tempDict = dict()
+  for card in cards:
+    if card in tempDict.keys():
+      tempDict[card]['numCopies'] = tempDict[card]['numCopies'] + 1
+    else:
+      tempDict[card] = {'numCopies':1}
+    
+  # Save format:
+  # Array of dicts,
+  # dict fields:
+  #   card (same representation as given in the input array)
+  #   numCopies
+  saveStructure = []
+  for key in tempDict:
+    entry = {'card': key,
+             'numCopies': tempDict[key]['numCopies']}
+    saveStructure.append(entry)
+
+
+  text = json.dumps(saveStructure)
   f.write(text)
   f.close()
   print "Deck saved."
